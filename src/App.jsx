0286730686b8130a6083de6db365d9b9559511c8
sujalcharati <sujalc024@gmail.com>
2024-11-29 12:03:40 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@
 import './App.css'
 
 function App() {
-  
+async  function  generateAudio(){
+  const res = await fetch(" https://dog.ceo/api/breeds/list/all");
+  const data = await res.json();
+  console.log(data)
+  return data ;
+  }
 
   return (
     <div>
 
       <h3> Do you want to generate a podcast for this article ? </h3>
-      <button>click here</button>
+      <button onClick={generateAudio}>click here</button>
     </div>
          
   )
